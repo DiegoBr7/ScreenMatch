c349c89b76938f13d99e200d76dc904cc9ac1a60
Diego Brasileiro <diegobrasileiro8@gmail.com>
2024-12-09 09:26:05 -0300

    inicio do projeto spring

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,27 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.model.DadosSerie;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import service.ConsumoAPI;
+import service.ConverteDados;
+
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		ConsumoAPI consumoAPI = new ConsumoAPI();
+		var json = ConsumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json , DadosSerie.class);
+		System.out.println(dados);
+	}
+}
